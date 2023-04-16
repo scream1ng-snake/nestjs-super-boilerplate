2c1bbad96b608a22f350aa5e3d0c48cc47c298f2
lykasov_am <lykasov_am@lexema.ru>
2023-04-16 17:26:32 +0500

    FIX: move providers lib folder to lib && ADD: entities lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ProvidersModule } from 'docker/postgres/libs/providers/src';
+import { ProvidersModule } from 'libs/providers/src';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
