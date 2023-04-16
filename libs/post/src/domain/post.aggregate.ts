e7168f992e7628e896bf3bb362a6cc6161f0fa74
lykasov_am <lykasov_am@lexema.ru>
2023-04-16 19:51:24 +0500

    ADD: validator post

--- libs/post/src/domain/post.aggregate.ts
+++ libs/post/src/domain/post.aggregate.ts
@@ -1,14 +1,32 @@
 import { IPost } from "./post.interface";
 import { randomStringGenerator } from "@nestjs/common/utils/random-string-generator.util"
-import { AggregateRoot } from "@nestjs/cqrs";
+import { PostServices } from "./services";
+import { IsUUID, IsString, IsNotEmpty, IsBoolean, validateSync } from "class-validator";
+import { Exclude } from "class-transformer";
 
-export class PostAggregate extends AggregateRoot implements IPost {
+export class PostAggregate extends PostServices implements IPost {
+  @IsUUID()
   id: string = randomStringGenerator();
+
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   message: string;
+
+  @IsUUID()
   authorId: string;
+
+  @IsBoolean()
+  @Exclude()
   isPublished = false;
+
+  @IsString()
   createdAt = new Date().toISOString();
+
+  @IsString()
   updatedAt = new Date().toISOString();
 
   private constructor() {
@@ -17,6 +35,21 @@ export class PostAggregate extends AggregateRoot implements IPost {
 
   static create(post: Partial<IPost>) {
     const _post = new PostAggregate();
+    _post.setNotPublished();
+
+    _post.updatedAt = post?.id
+      ? new Date().toISOString()
+      : _post.updatedAt
+    
+    const errors = validateSync(_post, {
+      whitelist: true
+    })
+
+    if(errors.length) {
+      throw new Error('Post not valid')
+    }
+
+
     Object.assign(_post, post);
     return _post;
   }
---
+++ libs/post/src/domain/services/index.ts
@@ -0,0 +1,11 @@
+import { ISetNotPublished, SET_NOT_PUBLISHED } from "./set-not-published.case";
+import { AggregateRoot } from "@nestjs/cqrs";
+import { ISetPublished, SET_PUBLISHED } from "./set-published.case";
+
+export class PostServices 
+  extends AggregateRoot 
+  implements ISetNotPublished, ISetPublished 
+{
+  setPublished = SET_PUBLISHED
+  setNotPublished = SET_NOT_PUBLISHED
+}
