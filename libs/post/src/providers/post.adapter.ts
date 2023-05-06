117f19b7e69837f6b118623faf6b4614c92b9385
lykasov_am <lykasov_am@lexema.ru>
2023-05-07 01:17:40 +0500

    UPD: POstFacade. Realization of repository

---
+++ libs/post/src/providers/post.adapter.ts
@@ -0,0 +1,79 @@
+import { PostEntity } from "@app/entities";
+import { PaginationDto } from "@app/shared/dto";
+import { Injectable, Logger } from "@nestjs/common";
+import { BadRequestException } from "@nestjs/common/exceptions";
+import { InjectRepository } from "@nestjs/typeorm";
+import { plainToInstance } from "class-transformer";
+import { FindManyOptions, Repository } from "typeorm";
+import { IPost, PostAggregate } from "../domain";
+import { PostRepository } from "./post.repository";
+
+@Injectable()
+export class PostAdapter implements PostRepository {
+  private readonly logger = new Logger(PostAdapter.name)
+
+  constructor(
+    @InjectRepository(PostEntity)
+    private readonly postRepository: Repository<PostEntity>,
+  ) { }
+
+  async save(post: IPost): Promise<PostAggregate> {
+    if (post?.id) {
+      const existsPost = await this.findOne(post.id);
+      if (!existsPost) {
+        throw new BadRequestException(`Post by id ${post.id} not found`)
+      }
+      const { id, ...toUpdate } = post;
+      await this.postRepository.update({ id }, toUpdate);
+      return this.findOne(post.id);
+    }
+    const savedPost = await this.postRepository.save(post);
+    return PostAggregate.create(savedPost);
+  }
+
+  async findOne(id: string): Promise<PostAggregate | null> {
+    const existsPost = await this.postRepository
+      .findOneBy({ id })
+      .catch((err) => {
+        this.logger.error(err);
+        return null;
+      })
+    if (!existsPost) {
+      throw new BadRequestException(`Post by id ${id} not found`)
+    }
+    return PostAggregate.create(existsPost);
+  }
+
+  async findAll(pagination: PaginationDto): Promise<[PostAggregate[], number]> {
+    const { limit: take, offset: skip } = plainToInstance(
+      PaginationDto,
+      pagination
+    )
+    const options: FindManyOptions<PostEntity> = {
+      where: { isPublished: true },
+      order: { createdAt: 'DESC' },
+      take,
+      skip,
+    }
+    const [data, count] = await this.postRepository
+      .findAndCount(options)
+      .catch(err => {
+        this.logger.error(err)
+        return [[], 0] as [PostEntity[], number]
+      })
+    return [
+      data.map(post => PostAggregate.create(post)),
+      count
+    ]
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const result =  await this.postRepository
+      .delete({ id })
+      .catch(err => {
+        this.logger.error(err);
+        return false;
+      });
+    return !!result;
+  }
+}
--- libs/post/src/providers/post.repository.ts
+++ libs/post/src/providers/post.repository.ts
@@ -4,6 +4,6 @@ import { IPost, PostAggregate } from "../domain"
 export abstract class PostRepository {
   abstract save(post: IPost): Promise<PostAggregate>;
   abstract findOne(id: string): Promise<PostAggregate | null>;
-  abstract findAll(pagination: PaginationDto): Promise<[[PostAggregate], number]>;
+  abstract findAll(pagination: PaginationDto): Promise<[PostAggregate[], number]>;
   abstract delete(id: string): Promise<boolean>;
 }
