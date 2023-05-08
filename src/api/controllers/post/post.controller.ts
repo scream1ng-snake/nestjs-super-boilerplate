a2ab0a540130936377fc9bee5e19d863f128569a
lykasov_am <lykasov_am@lexema.ru>
2023-05-08 09:49:17 +0500

    ADD: create post api realization

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -1,9 +1,24 @@
+import { CurrentUser, ICurrentUser } from '@app/auth';
 import { PostFacade } from '@app/post/application-services';
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
+import { CreatePostDto } from './dto';
 
 @Controller('post')
 export class PostController {
   constructor(
     private readonly postFacade: PostFacade
   ) {}
+
+  @Post()
+  createPost(
+    // @CurrentUser() { userId }: ICurrentUser, todo
+    @Body() createPostDto: CreatePostDto
+  ) {
+    return this.postFacade.commands.createPost({
+      ...createPostDto,
+      authorId: randomStringGenerator()
+      // authorId: userId
+    })
+  }
 }
