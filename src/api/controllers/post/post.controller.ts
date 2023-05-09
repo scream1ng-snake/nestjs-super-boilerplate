f1df631047d5bd50a8d42d1a1a3e8a5703f074f2
lykasov_am <lykasov_am@lexema.ru>
2023-05-09 20:47:22 +0500

    ADD: delete post method

--- src/api/controllers/post/post.controller.ts
+++ src/api/controllers/post/post.controller.ts
@@ -4,10 +4,10 @@ import { PostAggregate } from '@app/post';
 import { PostFacade } from '@app/post/application-services';
 import { ResponseWithPagination } from '@app/shared';
 import { PaginationDto } from '@app/shared/dto';
-import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Patch, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { plainToInstance } from 'class-transformer';
 import { v4 as uuidv4 } from 'uuid';
-import { CreatePostDto } from './dto';
+import { CreatePostDto, UpdatePostDto } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('post')
@@ -52,5 +52,30 @@ export class PostController {
       data,
       total: count
     }
+  }
+
+  @Put()
+  async updatePost(
+    @CurrentUser() user: ICurrentUser,
+    @Body() updatePost: UpdatePostDto
+  ) {
+    return this.postFacade.commands.updatePost({
+      ...updatePost,
+      authorId: user.userId
+    })
+  }
+
+  @Patch(':id')
+  async setPublished(
+    @Param('id', ParseUUIDPipe) id: string
+  ) {
+    return this.postFacade.commands.setPublished(id);
+  }
+
+  @Delete(':id')
+  async deletePost(
+    @Param('id', ParseUUIDPipe) id: string
+  ) {
+    return this.postFacade.commands.deletePost(id)
   }
 }
