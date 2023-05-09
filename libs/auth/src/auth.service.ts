5cd8ff7558e503cdf6952d6cc3e5b009e03b44f9
lykasov_am <lykasov_am@lexema.ru>
2023-05-09 19:01:49 +0500

    ADD: jwt strategy

--- libs/auth/src/auth.service.ts
+++ libs/auth/src/auth.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async validateUser(email: string): Promise<boolean> {
+    return email == 'test@mail.ru'
+  }
+}
---
+++ libs/auth/src/strategies/index.ts
@@ -0,0 +1,5 @@
+import { JwtStrategy } from "./jwt.strategy";
+
+export const STRATEGIES = [
+  JwtStrategy
+]
