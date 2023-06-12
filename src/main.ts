e1903d35311a66e2d90b96d4dddf35ccf3cea453
lykasov_am <lykasov_am@lexema.ru>
2023-06-12 14:16:57 +0500

    BOOTSTRAP: add log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000)
+  const config = new DocumentBuilder()
+    .setTitle('POSTS')
+    .setDescription('CRUD API by posts')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
+  await app.listen(port, () => {
+    Logger.log(`Application started at http://localhost:${port}`, 'Main')
+    Logger.log(`Swagger docs at http://localhost:${port}/api-doc`, 'Main')
+  });
 }
 bootstrap();
